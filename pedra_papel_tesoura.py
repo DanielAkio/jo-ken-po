1bb900e34c59dd276ae179334605a0c87e47b694
Daniel Akio Teixeira <danielakioteixeira@gmail.com>
2020-11-04 11:02:54 -0300

    Testando formatador black

--- pedra_papel_tesoura.py
+++ pedra_papel_tesoura.py
@@ -3,25 +3,46 @@ def main():
     mostra_qual_jogo_esta_sendo_jogado()
     primeiro_jogador = qual_nome_do_primeiro_jogador()
     segundo_jogador = qual_nome_do_segundo_jogador()
-    while(jogar_novamente == "1"):
+    while jogar_novamente == "1":
         um_monte_de_espaco()
-        jogada_do_primeiro_jogador = validar_jogada(pergunta_jogada_do_primeiro_jogador(primeiro_jogador))
-        while(not jogada_do_primeiro_jogador):
-            jogada_do_primeiro_jogador = validar_jogada(pergunta_jogada_do_primeiro_jogador(primeiro_jogador))
+        jogada_do_primeiro_jogador = validar_jogada(
+            pergunta_jogada_do_primeiro_jogador(primeiro_jogador)
+        )
+        while not jogada_do_primeiro_jogador:
+            jogada_do_primeiro_jogador = validar_jogada(
+                pergunta_jogada_do_primeiro_jogador(primeiro_jogador)
+            )
         um_monte_de_espaco()
-        jogada_do_segundo_jogador = validar_jogada(pergunta_jogada_do_segundo_jogador(segundo_jogador))
-        while(not jogada_do_segundo_jogador):
-            jogada_do_segundo_jogador = validar_jogada(pergunta_jogada_do_segundo_jogador(segundo_jogador))
+        jogada_do_segundo_jogador = validar_jogada(
+            pergunta_jogada_do_segundo_jogador(segundo_jogador)
+        )
+        while not jogada_do_segundo_jogador:
+            jogada_do_segundo_jogador = validar_jogada(
+                pergunta_jogada_do_segundo_jogador(segundo_jogador)
+            )
         um_monte_de_espaco()
-        jogada_do_primeiro_jogador_palavra = qual_o_primeiro_jogador_jogou(jogada_do_primeiro_jogador)
-        jogada_do_segundo_jogador_palavra = qual_o_segundo_jogador_jogou(jogada_do_segundo_jogador)
-        mostra_o_que_cada_um_jogou(primeiro_jogador, segundo_jogador, jogada_do_primeiro_jogador_palavra, jogada_do_segundo_jogador_palavra)
-        quem_ganhou(primeiro_jogador, segundo_jogador, jogada_do_primeiro_jogador_palavra, jogada_do_segundo_jogador_palavra)
+        jogada_do_primeiro_jogador_palavra = qual_o_primeiro_jogador_jogou(
+            jogada_do_primeiro_jogador
+        )
+        jogada_do_segundo_jogador_palavra = qual_o_segundo_jogador_jogou(
+            jogada_do_segundo_jogador
+        )
+        mostra_o_que_cada_um_jogou(
+            primeiro_jogador,
+            segundo_jogador,
+            jogada_do_primeiro_jogador_palavra,
+            jogada_do_segundo_jogador_palavra,
+        )
+        quem_ganhou(
+            primeiro_jogador,
+            segundo_jogador,
+            jogada_do_primeiro_jogador_palavra,
+            jogada_do_segundo_jogador_palavra,
+        )
         jogar_novamente = quer_jogar_novamente()
         if jogar_novamente == "1":
             um_monte_de_espaco()
     mensagem_de_adeus()
-
 
 
 def mostra_qual_jogo_esta_sendo_jogado():
@@ -30,20 +51,30 @@ def mostra_qual_jogo_esta_sendo_jogado():
     print("*** Voce vai jogar jo-ken-po! ***")
     print("*********************************")
 
+
 def qual_nome_do_primeiro_jogador():
     return input("\nQual o nome do primeiro jogador?\n")
+
 
 def qual_nome_do_segundo_jogador():
     return input("\nQual o nome do segundo jogador?\n")
 
+
 def um_monte_de_espaco():
     print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
 
+
 def pergunta_jogada_do_primeiro_jogador(primeiro_jogador):
-    return input(f"\n{primeiro_jogador} escolha entre: \n\n(1) Pedra \n(2) Papel \n(3) Tesoura\n\n")
+    return input(
+        f"\n{primeiro_jogador} escolha entre: \n\n(1) Pedra \n(2) Papel \n(3) Tesoura\n\n"
+    )
+
 
 def pergunta_jogada_do_segundo_jogador(segundo_jogador):
-    return input(f"\n{segundo_jogador} escolha entre: \n\n(1) Pedra \n(2) Papel \n(3) Tesoura\n\n")
+    return input(
+        f"\n{segundo_jogador} escolha entre: \n\n(1) Pedra \n(2) Papel \n(3) Tesoura\n\n"
+    )
+
 
 def qual_o_primeiro_jogador_jogou(jogada_do_primeiro_jogador):
     if jogada_do_primeiro_jogador == 1:
@@ -54,6 +85,7 @@ def qual_o_primeiro_jogador_jogou(jogada_do_primeiro_jogador):
         jogada = "Tesoura"
     return jogada
 
+
 def qual_o_segundo_jogador_jogou(jogada_do_segundo_jogador):
     if jogada_do_segundo_jogador == 1:
         jogada = "Pedra"
@@ -63,21 +95,48 @@ def qual_o_segundo_jogador_jogou(jogada_do_segundo_jogador):
         jogada = "Tesoura"
     return jogada
 
-def mostra_o_que_cada_um_jogou(primeiro_jogador, segundo_jogador, jogada_do_primeiro_jogador, jogada_do_segundo_jogador):
-    print(f"O {primeiro_jogador} jogou {jogada_do_primeiro_jogador} e o {segundo_jogador} jogou {jogada_do_segundo_jogador}\n")
 
-def quem_ganhou(primeiro_jogador, segundo_jogador, jogada_do_primeiro_jogador, jogada_do_segundo_jogador):
+def mostra_o_que_cada_um_jogou(
+    primeiro_jogador,
+    segundo_jogador,
+    jogada_do_primeiro_jogador,
+    jogada_do_segundo_jogador,
+):
+    print(
+        f"O {primeiro_jogador} jogou {jogada_do_primeiro_jogador} e o {segundo_jogador} jogou {jogada_do_segundo_jogador}\n"
+    )
+
+
+def quem_ganhou(
+    primeiro_jogador,
+    segundo_jogador,
+    jogada_do_primeiro_jogador,
+    jogada_do_segundo_jogador,
+):
     if jogada_do_primeiro_jogador == jogada_do_segundo_jogador:
         print("Empate")
-    elif (jogada_do_primeiro_jogador == "Pedra" and jogada_do_segundo_jogador == "Tesoura") or \
-       (jogada_do_primeiro_jogador == "Papel" and jogada_do_segundo_jogador == "Pedra") or \
-       (jogada_do_primeiro_jogador == "Tesoura" and jogada_do_segundo_jogador == "Papel"):
+    elif (
+        (
+            jogada_do_primeiro_jogador == "Pedra"
+            and jogada_do_segundo_jogador == "Tesoura"
+        )
+        or (
+            jogada_do_primeiro_jogador == "Papel"
+            and jogada_do_segundo_jogador == "Pedra"
+        )
+        or (
+            jogada_do_primeiro_jogador == "Tesoura"
+            and jogada_do_segundo_jogador == "Papel"
+        )
+    ):
         print(f"O {primeiro_jogador} ganhou!!")
     else:
         print(f"O {segundo_jogador} ganhou!!")
 
+
 def quer_jogar_novamente():
     return input(f"\nDeseja jogar novamente boiolao? \n\n(1) Sim \n(2) Nao\n")
+
 
 def validar_jogada(jogada):
     if jogada == "":
@@ -89,11 +148,13 @@ def validar_jogada(jogada):
         return False
     return jogada
 
+
 def mensagem_de_adeus():
     print("\nObrigado por jogar, esperamos que vc tenha se divertido!\n")
     print("\nCriadores:")
     print("- Felipe Maeta dos Santos")
     print("- Daniel Akio Teixeira")
+
 
 if __name__ == "__main__":
     main()
